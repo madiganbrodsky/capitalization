97d80baee96284de66a517c03e8e03e534f23f83
Sebastian Schuster <sebschu@gmail.com>
2022-08-26 07:24:24 +0200

    debugging

--- experiments/clicking/js/experiment.js
+++ experiments/clicking/js/experiment.js
@@ -11,41 +11,63 @@ function make_slides(f) {
   });
 
   // set up the first example slide
-  slides.example1 = slide({
-    name: "example1",
+  slides.trial = slide({
+    name: "trial",
+   
+    // exp.stimuli stores the stimuli for each trial
+    present: exp.stimuli,
+    // present_handle gets called for each stimulus in exp.stimuli
+    present_handle : function(stim) {
 
-    // this is executed when the slide is shown
-    start: function() {
-      // hide error message
-      $('.err').hide();
-    },
+      //record trial start time
+      this.trial_start = Date.now();
 
-    //this is executed when the participant clicks the Continue button
-    button: function() {
+      //clear all content from "#words"
+      $("#words").html("");
+
       // this delimits the string by spaces
-      "words".split(" ")
+      var words = "these are some words".split(" "); //should be replaced with data from stim
       // for each word in the string, create a span element and display it
       $.each(words, function(idx, value) {
-        $("words").append($("<span>").text(value))
-        this.index = value.data(idx)
-      },
+        $("#words").append(
+          $("<span>")
+            .text(value)
+            .data("index", idx))
+          .append(" "); // append space after each word
+      });
+
+
       //when a word (span) is clicked, it will become uppercase and the index of that word will be recorded
-      $("words").click(function() {
-          jQuery(this).toUpperCase;
-          this.log_responses();
-          exp.go();
-      },
+      $("#words span").click(function() {
+          var elm = $(this);
+          // check whether word is currently capitalized by checking whether first character is uppercase
+          var isUpper = elm.text()[0] == elm.text()[0].toUpperCase();
+          if (isUpper) {
+            //make the entire word lowercase (= making the first character lowercase)
+            elm.text(elm.text().toLowerCase());
+          } else {
+            //make the first character uppercase
+            elm.text(elm.text()[0].toUpperCase() + elm.text().substr(1));
+          }
+          _s.log_responses(elm, isUpper);
+      });
     },
 
-    log_responses: function() {
+    log_responses: function(elm, isUpper) {
       // add response to exp.data_trials
       // this data will be submitted at the end of the experiment
       exp.data_trials.push({
         "slide_number_in_experiment": exp.phase,
-        "id": "example1",
-        "response": this.index,
+        "index": elm.data("index"),
+        "word": elm.text().toLowerCase(),
+        "response_time": Date.now() - this.trial_start,
+        "operation": isUpper ? -1 : 1 // 1: capitalize, -1: decapitalize  
       });
     },
+
+    button: function() {
+      _stream.apply(this);
+    }
   });
 
   // set up slide for second example trial
@@ -90,79 +112,7 @@ function make_slides(f) {
     },
   });
 
-  slides.trial = slide({
-    name: "trial",
-
-    // start: function() {
-    //   var stim = {
-    //     "TGrep": "37224:9",
-    //     "Context": "Speaker A:  and, and i, you know, i still provide most of the things that  go on around the house.<p>Speaker B: right.<p>Speaker A: so, uh, yeah and for a while i was going to school too, and tha-, it was tough.<p>Speaker B: yeah,  i uh, i think that while it 's a good change for i think women to be able  to fulfill their potential in whatever they feel, you know, their expertise may be .<p>Speaker A: uh-huh.<p>Speaker B: uh-huh.<p>Speaker A: uh, i think sometimes other things suffer and tha-, i think it 's hard to find a balance there.<p>Speaker B: ",
-    //     "EntireSentence": "but in some ways i think we are expected  to do it all.",
-    //     "ButNotAllSentence": "but in <strong>some, but not all</strong> ways i think we are expected  to do it all."
-    //   }
-    // The 7 lines above from "start:..." to the end of var stim = {...}" define a placeholder stimulus that you will have to delete when
-    // loading in the individual stimulus data.
-
-    // To rotate through stimulus list, comment out the above 7 lines and  uncomment the following 2:
-    present: exp.stimuli,
-    present_handle : function(stim) {
-
-      // unselect all radio buttons at the beginning of each trial
-      // (by default, the selection of the radio persists across trials)
-      $("input[name='number']:checked").prop("checked", false);
-      $("#check-strange").prop("checked", false);
-
-      // store stimulus data
-      this.stim = stim;
-
-      // extract original and sentence with "but not all"
-      var original_sentence = stim.EntireSentence;
-      var target_sentence = stim.ButNotAllSentence;
-
-      //handle display of context
-      // if (exp.condition == "context") {
-      //   // extract context data
-      //   var contexthtml = stim.Context;
-      //   // reformat the speaker information for context
-      //   contexthtml = contexthtml.replace(/Speaker A:/g, "<b>Speaker #1:</b>");
-      //   contexthtml = contexthtml.replace(/Speaker B:/g, "<b>Speaker #2:</b>");
-      //   $(".case").html(contexthtml);
-      // } else {
-      //   var contexthtml = "";
-      //   $(".case").html(contexthtml);
-      // }
-
-      // replace the placeholder in the HTML document with the relevant sentences for this trial
-      $("#trial-originalSen").html(original_sentence);
-      $("#trial-targetSen").html(target_sentence);
-      $(".err").hide();
-
-    },
-
-    // handle click on "Continue" button
-    button: function() {
-      this.radio = $("input[name='number']:checked").val();
-      this.strange = $("#check-strange:checked").val() === undefined ? 0 : 1;
-      if (this.radio) {
-        this.log_responses();
-        // exp.go(); //use exp.go() if and only if there is no "present"ed data, ie no list of stimuli.
-        _stream.apply(this); //use _stream.apply(this) if there is a list of "present" stimuli to rotate through
-      } else {
-        $('.err').show();
-      }
-    },
-
-    // save response
-    log_responses: function() {
-      exp.data_trials.push({
-        "id": this.stim.TGrep,
-        "sentence": this.stim.ButNotAllSentence,
-        "slide_number_in_experiment": exp.phase, //exp.phase is a built-in trial number tracker
-        "response": this.radio,
-        "strangeSentence": this.strange
-      });
-    },
-  });
+  
 
   // slide to collect subject information
   slides.subj_info = slide({
@@ -225,9 +175,6 @@ function init() {
   //blocks of the experiment:
   exp.structure = [
     "i0",
-    "example1",
-    "example2",
-    "startExp",
     "trial",
     "subj_info",
     "thanks"
